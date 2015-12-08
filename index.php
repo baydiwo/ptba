9edc17dd3612bdfc7ef0948b5475cb48e9559a95
Bayu Adi Wibowo <baydiwo@gmail.com>
2015-12-08 12:14:33 +0700

    approved homepage

---
+++ index.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>PTBA</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery.fullPage.css" rel="stylesheet" type="text/css" />
+    <link href="css/animate.css" rel="stylesheet" media="screen">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!--[if IE]>
+        <script type="text/javascript">
+             var console = { log: function() {} };
+        </script>
+    <![endif]-->
+
+</head>
+<body>
+
+    <header>
+        <!-- slider -->
+        <div id="fullpage" class="">
+            <div class="section " id="section0">
+                <div class="slide" id="slide1">
+                    <div class="caption">
+                        <div class="title">
+                            <h2>Work Smart. Work Hard.</h2>
+                        </div>
+                        <div class="description">
+                            A WORLD-CLASS ENVIRONMENT-CARE ENERGY COMPANY.
+                        </div>
+                    </div>
+                </div>
+                <div class="slide" id="slide1">
+                    <div class="caption">
+                        <div class="title">
+                            <h2>Work Smart. Work Hard.</h2>
+                        </div>
+                        <div class="description">
+                            A WORLD-CLASS ENVIRONMENT-CARE ENERGY COMPANY.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="slide" id="slide1">
+                    <div class="caption">
+                        <div class="title">
+                            <h2>Work Smart. Work Hard.</h2>
+                        </div>
+                        <div class="description">
+                            A WORLD-CLASS ENVIRONMENT-CARE ENERGY COMPANY.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end of slider -->
+        <div class="container-fluid">
+            <div class="row logo-nav">
+                <div class="col-lg-2">
+                    <div class="logo wow fadeIn">
+                        <a href="#" title="PT Bukit Asam"><img src="img/logo.png" alt="PT Bukit Asam"></a>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <div class="row">
+                        <div class="col-lg-3 pull-right">
+                            <a href="" class=" pull-right"><img src="img/id.png" height="22" width="32" alt="" class="pull-right img-responsive lang"></a>
+                            <form class="form-inline pull-right">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input type="search" class="form-control input-sm" id="search" placeholder="Search...">
+                                        <div class="input-group-addon"><button type="submit" class=""><span class="glyphicon glyphicon-search"></span></button></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+                    <nav class="navbar navbar-default navbar-right">
+                        <div class="container-fluid">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                            </div>
+
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                <ul class="nav navbar-nav">
+                                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                                    <li><a href="#">Link</a></li>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">Action</a></li>
+                                            <li><a href="#">Another action</a></li>
+                                            <li><a href="#">Something else here</a></li>
+                                            <li role="separator" class="divider"></li>
+                                            <li><a href="#">Separated link</a></li>
+                                            <li role="separator" class="divider"></li>
+                                            <li><a href="#">One more separated link</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Link</a></li>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">Action</a></li>
+                                            <li><a href="#">Another action</a></li>
+                                            <li><a href="#">Something else here</a></li>
+                                            <li role="separator" class="divider"></li>
+                                            <li><a href="#">Separated link</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div><!-- /.navbar-collapse -->
+                        </div><!-- /.container-fluid -->
+                    </nav>
+                </div>
+            </div>
+            <!-- end of row logo nav -->
+            <!-- accordion start -->
+            <div class="accordion wow slideInRight">
+                <img src="img/widget1.png" height="103" width="372" alt="" style="margin-bottom: 10px;">
+                <div class="widget">
+                    <h3>ANNUAL REPORT<span class="st-arrow"></h3>
+                    <div class="pane">
+                        <span><img src="img/dummy-list.png" height="20" width="268" alt=""></span><br>
+                        <span><img src="img/dummy-list.png" height="20" width="268" alt=""></span><br>
+                    </div>
+                </div>
+                <div class="widget">
+                    <h3>audit financial report<span class="st-arrow"></h3>
+                    <div class="pane">
+                        <span><img src="img/dummy-list.png" height="20" width="268" alt=""></span><br>
+                        <span><img src="img/dummy-list.png" height="20" width="268" alt=""></span><br>
+                    </div>
+                </div>
+                <div class="widget">
+                    <h3>company presentation<span class="st-arrow"></h3>
+                    <div class="pane">
+                        <img src="img/dummy-list2.png" height="91" width="285" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- accordion end -->
+        </div>
+    </header>
+    <article>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 no-padding">
+                    <div class="post wow slideInUp">
+                        <div class="view view-first">
+                            <img src="img/dummy-img1.jpg" class="img-article img-responsive"/>
+                            <div class="mask">
+                                <div class="line">
+                                    <img class="img-icon" src="img/icon-news.png" alt="">
+                                    <p>19 November 2015</p>
+                                    <h2>PTBA Field Study Place for Forestry</h2>
+                                    <div class="divider"><img src="img/divider.png"></div>
+                                    <a href="#" class="info">see all news</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 no-padding">
+                    <div class="post wow slideInUp">
+                        <div class="view view-first">
+                            <img src="img/dummy-img1.jpg" height="412" width="620" class="img-article img-responsive" />
+                            <div class="mask">
+                                <div class="line">
+                                    <img class="img-icon" src="img/icon-light.png">
+                                    <p>19 November 2015</p>
+                                    <h2>PTBA Field Study Place for Forestry</h2>
+                                    <div class="divider"><img src="img/divider.png"></div>
+                                    <a href="#" class="info">see all highlights</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 no-padding">
+                    <div class="post wow slideInUp">
+                        <div class="view view-first">
+                            <img src="img/dummy-img1.jpg" height="412" width="620" class="img-article img-responsive" />
+                            <div class="mask">
+                                <div class="line">
+                                    <img class="img-icon" src="img/icon-archive.png">
+                                    <p>19 November 2015</p>
+                                    <h2>PTBA Field Study Place for Forestry</h2>
+                                    <div class="divider"><img src="img/divider.png"></div>
+                                    <a href="#" class="info">see all memories</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 no-padding">
+                    <div class="post fb-feed wow slideInUp">
+                        <div class="view view-first">
+                            <img src="img/dummy-img1.jpg" height="412" width="620" class="img-article img-responsive" />
+                            <div class="mask">
+                                <div class="line">
+                                    <img class="img-icon" src="img/icon-fbfeed.png">
+                                    <p>25 Nov 2015
+                                    PTBA kembali raih PROPER EMAS tahun 2015. Penghargaan ini merupakan yang ke-3 kalinya, setelah sebelumnya memperoleh penghargaan yang sama di tahun 2013 dan 2014. PTBA adalah ...</p>
+
+                                    <p>24 Nov 2015
+                                    Jumat (9/10) malam, PTBA gelar syukuran karena telah resmi menggunakan Ellips 8.5.3. di auditorium lantai IV GSG. Ellips adalah program aplikasi komputer yang dipakai PTBA dal ...</p>
+                                    <div class="divider"><img src="img/divider.png"></div>
+                                    <a href="#" class="info">visit our facebook page</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <article>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 no-padding">
+                    <div class="banner">
+                        <img src="img/banner.jpg" height="244" width="1280" alt="" class="img-responsive">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-2">
+                    <h5>about us</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="">Company Profile</a></li>
+                        <li><a href="">Organization Structure</a></li>
+                        <li><a href="">From CEO Desk</a></li>
+                        <li><a href="">Good Corporate </a></li>
+                        <li><a href="">Governance</a></li>
+                        <li><a href="">Whistle Blowing System</a></li>
+                        <li><a href="">Our Business</a></li>
+                        <li><a href="">Awards and Certifications</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <h5>news</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="">Articles</a></li>
+                        <li><a href="">CSR News & Activities</a></li>
+                        <li><a href="">Press Release</a></li>
+                        <li><a href="">Highlight</a></li>
+                        <li><a href="">Memories</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <h5>CSR</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="">About CSR</a></li>
+                        <li><a href="">Sustainable Report</a></li>
+                        <li><a href="">CSR News & Activities</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <h5>INVESTOR RELATIONS</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="">Shareholder Information</a></li>
+                        <li><a href="">Company Report</a></li>
+                        <li><a href="">Company Presentation</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <h5>library</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="">Knowledge Center</a></li>
+                        <li><a href="">Regulations</a></li>
+                        <li><a href="">Ministerial Regulations</a></li>
+                        <li><a href="">BEI Regulations</a></li>
+                        <li><a href="">OJK Regulations</a></li>
+                        <li><a href="">PTBA Regulations</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <h5>ENVIRONMENT</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="">Watershed Rehabilitation</a></li>
+                        <li><a href="">Environment</a></li>
+                        <li><a href="">Post-Mining</a></li>
+                        <li><a href="">Reclamation</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row footer-bottom">
+                <div class="col-lg-4">
+                    <ul class="list-unstyled list-inline list-footer-bottom">
+                        <li><a href="">e-procurement</a></li>
+                        <li><a href="">jamkes</a></li>
+                        <li><a href="">career</a></li>
+                        <li><a href="">contact us</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-1 no-padding">
+                    <ul class="list-unstyled list-inline list-icon">
+                        <li><a href=""><i class="fa fa-facebook-square fa-3"></i></a></li>
+                        <li><a href=""><i class="fa fa-youtube-square fa-3"></i></a></li>
+                        <li><a href=""><i class="fa fa-linkedin-square fa-3"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <p>Do you have a question? <br> Please contact Us  021.525.4014</p>
+                </div>
+                <div class="col-lg-3">
+                    <p>Copyright © 2014 - 2016 PT Bukit Asam (Persero) Tbk</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="js/hoverIntent.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.slimscroll.min.js"></script>
+    <script type="text/javascript" src="js/jquery.fullPage.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easings.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+</body>
+</html>
