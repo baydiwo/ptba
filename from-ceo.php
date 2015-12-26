f1ac7a4f06ccd572d2c46ae0ccdffda7d75b0a03
Bayu Adi Wibowo <baydiwo@gmail.com>
2015-12-27 03:46:44 +0700

    +from CEO +organization

---
+++ from-ceo.php
@@ -0,0 +1,147 @@
+<?php include"inc/head.php" ?>
+
+    <header>
+        <?php include "inc/logo-nav.php" ?>
+        <!-- end of row logo nav -->
+            <section>
+                <div class="article-header">
+                    <img src="img/header-about-us.jpg" height="400" width="1280" class="img-responsive">
+                    <div class="caption">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6"><h1>about us</h1></div>
+                                <div class="col-lg-6"><h2 class="pull-right">company profile</h2></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </header>
+
+    <article class="article">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="title" id="history">
+                         <h2><span>PT BUKIT ASAM (PERSERO) TBK <br> CEO MESSAGES ON GREETING 2015 PERFORMANCE</span></h2>
+                     </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="content">
+                        <p>Coal business in 2014 was still slowing down as reflected in its significant fallen price since mid 2012. Many main coal players were lost, even closed down the business. On this condition, PTBA is still exist, even in better condition than previous year in whole performance, as in operational, finance and share price. It was an evidence that poor condition to its peer companies was a good opportunity for PTBA.</p>
+                        <p>What is the key for PTBA success? PTBA has taken strategic steps to incrase its efficiency in all lines and to supress base production cost (HPP) optimally. Specifically in mining process, the strategic steps are to mine more high calories coals, shortening the coal transportation within mine area and to put priority on using electricity generated from PTBA own 3 x 10 Megawatts steam based power plant (PLTU) in Tanjung Enim. Likewise, Tarahan Port electricity is already generated from another PTBA 2 x 8 Megawatts PLTU.</p>
+                    </div>
+                    <div class="quotes-2">
+                         <aside>
+                             Financial performance in 2014, January to September (unauditted) shown PTBA sucessfully earned Net Profit of IDR 1,58 trilion or 27% higher than on the same period in the previous year (IDR 1,24 trillion). With its 16 % of Net Profit Margin (NPM), PTBA was the highest earner in Indonesia coal industry.<br>
+                             <span>- PT Bukit Asam</span>
+                         </aside>
+                     </div>
+                     <div class="quotes no-shadow left-line">
+                        <aside>
+                             Not only owned a high caliber power plant, but PTBA also saved from high cost electricity and minimized higher price non-subsidized fuels usage. Evenmore, PTBA was benefited from excess power sales to PT. Perusahaan Listrik Negara (PLN) of both PLTU.
+                        </aside>
+                     </div>
+                     <div class="content">
+                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dignissimos sed repellendus iusto reiciendis eligendi facere? Animi soluta maxime, modi itaque voluptas, veniam, id officia, quaerat voluptates unde facere molestiae.</p>
+                     </div>
+                </div>
+
+                <div class="row org-list">
+                    <div class="col-lg-3">
+                        <div class="people">
+                            <img src="img/comm.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <h3>Agus Suhartono</h3>
+                        <span class="pos">President Commissioner / Independent Commissioner</span>
+                        <p>
+                            Menjabat sebagai Komisaris Utama sejak RUPSLB pada tanggal 10 Oktober 2013 dengan latar belakang pendidikan di Lemhanas (2003), Sesko TNI (1999), Seskoal (1994) dan Akademi Angkatan Laut (1978). <br><br>
+                            Sebelum menjabat sebagai Komisaris Utama PT Bukit Asam Tbk, pria kelahiran Blitar 25 Agustus 1955 ini pernah menjabat sebagai Panglima TNI periode 2010 s/d 2013. Sebelumnya beberapa jabatan penting juga pernah dijalaninya, seperti Kepala Staf Angkatan Laut (KSAL) di tahun 2010 dan menjadi Inspektur Jenderal Departemen Pertahanan.
+                        </p>
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div><!-- container -->
+
+        <div id="fullpage">
+            <div class="section location">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="title">
+                                 <h2><span>vision &amp; mission</span></h2>
+                             </div>
+                        </div>
+                        <div class="col-lg-8 col-lg-offset-2">
+                            <div class="map">
+                                <img src="img/map.jpg" height="411" width="970" alt="" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section subsdiaries">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="title">
+                                 <h2><span>vision &amp; mission</span></h2>
+                             </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <ul class="carousel">
+                                <li>
+                                    <img src="img/subdiaries1.png" height="90" width="190" alt="">
+                                    <h3 class="single-word">Work sincere</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto earum reprehenderit saepe molestiae, nesciunt expedita, harum quidem voluptas consequatur consectetur, dolore mollitia quos, quae soluta ut nulla officia maiores cum.</p>
+                                </li>
+                                <li>
+                                    <img src="img/subdiaries1.png" height="90" width="190" alt="">
+                                    <h3 class="single-word">Work sincere</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto earum reprehenderit saepe molestiae, nesciunt expedita, harum quidem voluptas consequatur consectetur, dolore mollitia quos, quae soluta ut nulla officia maiores cum.</p>
+                                </li>
+                                <li>
+                                    <img src="img/subdiaries1.png" height="90" width="190" alt="">
+                                    <h3 class="single-word">Work sincere</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto earum reprehenderit saepe molestiae, nesciunt expedita, harum quidem voluptas consequatur consectetur, dolore mollitia quos, quae soluta ut nulla officia maiores cum.</p>
+                                </li>
+                                <li>
+                                    <img src="img/subdiaries1.png" height="90" width="190" alt="">
+                                    <h3 class="single-word">Work sincere</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto earum reprehenderit saepe molestiae, nesciunt expedita, harum quidem voluptas consequatur consectetur, dolore mollitia quos, quae soluta ut nulla officia maiores cum.</p>
+                                </li>
+                                <li>
+                                    <img src="img/subdiaries1.png" height="90" width="190" alt="">
+                                    <h3 class="single-word">Work sincere</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto earum reprehenderit saepe molestiae, nesciunt expedita, harum quidem voluptas consequatur consectetur, dolore mollitia quos, quae soluta ut nulla officia maiores cum.</p>
+                                </li>
+                                <li>
+                                    <img src="img/subdiaries1.png" height="90" width="190" alt="">
+                                    <h3 class="single-word">Work sincere</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto earum reprehenderit saepe molestiae, nesciunt expedita, harum quidem voluptas consequatur consectetur, dolore mollitia quos, quae soluta ut nulla officia maiores cum.</p>
+                                </li>
+                                <li>
+                                    <img src="img/subdiaries1.png" height="90" width="190" alt="">
+                                    <h3 class="single-word">Work sincere</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto earum reprehenderit saepe molestiae, nesciunt expedita, harum quidem voluptas consequatur consectetur, dolore mollitia quos, quae soluta ut nulla officia maiores cum.</p>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </article>
+
+
+    <?php include "inc/footer.php"; ?>
---
+++ organization.php
@@ -0,0 +1,78 @@
+<?php include"inc/head.php" ?>
+
+    <header>
+        <?php include "inc/logo-nav.php" ?>
+        <!-- end of row logo nav -->
+            <section>
+                <div class="article-header" style="background:url(img/header-about-us.jpg) no-repeat center center;">
+                    <div class="caption">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6"><h1>about us</h1></div>
+                                <div class="col-lg-6"><h2 class="pull-right">company profile</h2></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </header>
+
+    <article class="article">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="title" id="history">
+                         <h2><span>ORGANIZATION STRUCTURE</span></h2>
+                     </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="quotes no-shadow">
+                         <aside>
+                             PTBA would like to keep growing and developing with the surrounding community, building harmonious relationships in sustainable environment and giving most benefit to meet stakeholders' expectations.<br>
+                             <span>- PT Bukit Asam</span>
+                         </aside>
+                     </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="title" id="history">
+                         <h2><span>ORGANIZATION STRUCTURE</span></h2>
+                     </div>
+                </div>
+                <div class="row org-list">
+                    <div class="col-lg-4">
+                        <div class="people">
+                            <img src="img/comm.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="col-lg-8">
+                        <h3>Agus Suhartono</h3>
+                        <span class="pos">President Commissioner / Independent Commissioner</span>
+                        <p>
+                            Menjabat sebagai Komisaris Utama sejak RUPSLB pada tanggal 10 Oktober 2013 dengan latar belakang pendidikan di Lemhanas (2003), Sesko TNI (1999), Seskoal (1994) dan Akademi Angkatan Laut (1978). <br><br>
+                            Sebelum menjabat sebagai Komisaris Utama PT Bukit Asam Tbk, pria kelahiran Blitar 25 Agustus 1955 ini pernah menjabat sebagai Panglima TNI periode 2010 s/d 2013. Sebelumnya beberapa jabatan penting juga pernah dijalaninya, seperti Kepala Staf Angkatan Laut (KSAL) di tahun 2010 dan menjadi Inspektur Jenderal Departemen Pertahanan.
+                        </p>
+                    </div>
+                </div>
+                <div class="row org-list">
+                    <div class="col-lg-3">
+                        <div class="people">
+                            <img src="img/comm.jpg" class="img-responsive">
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <h3>Agus Suhartono</h3>
+                        <span class="pos">President Commissioner / Independent Commissioner</span>
+                        <p>
+                            Menjabat sebagai Komisaris Utama sejak RUPSLB pada tanggal 10 Oktober 2013 dengan latar belakang pendidikan di Lemhanas (2003), Sesko TNI (1999), Seskoal (1994) dan Akademi Angkatan Laut (1978). <br><br>
+                            Sebelum menjabat sebagai Komisaris Utama PT Bukit Asam Tbk, pria kelahiran Blitar 25 Agustus 1955 ini pernah menjabat sebagai Panglima TNI periode 2010 s/d 2013. Sebelumnya beberapa jabatan penting juga pernah dijalaninya, seperti Kepala Staf Angkatan Laut (KSAL) di tahun 2010 dan menjadi Inspektur Jenderal Departemen Pertahanan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- container -->
+    </article>
+
+
+    <?php include "inc/footer.php"; ?>
