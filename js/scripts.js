b3637ce944a385631f84a86e1ea35121cc5a4d4b
Bayu Adi Wibowo <baydiwo@gmail.com>
2016-01-11 21:20:06 +0700

    fixing jquery conflict

--- js/scripts.js
+++ js/scripts.js
@@ -18,77 +18,6 @@ $(document).ready(function() {
       prevText: " ",
     });
 
-    // $('.news-slider').bxSlider({
-    //     pagerCustom: '#news-pager',
-    //     controls: false,
-    //     captions: true,
-    // });
-     var $j = jQuery.noConflict();
-
-var realSlider= $j("ul#bxslider").bxSlider({
-      speed:1000,
-      pager:false,
-      nextText:'',
-      prevText:'',
-      infiniteLoop:false,
-      hideControlOnEnd:true,
-      controls:false,
-      captions:true,
-      auto:true,
-      onSlideBefore:function($slideElement, oldIndex, newIndex){
-        changeRealThumb(realThumbSlider,newIndex);
-      }
-
-    });
-
-    var realThumbSlider=$j("ul#bxslider-pager").bxSlider({
-      minSlides: 4,
-      maxSlides: 4,
-      slideWidth: 156,
-      slideMargin: 12,
-      moveSlides: 1,
-      pager:false,
-      speed:1000,
-      infiniteLoop:false,
-      hideControlOnEnd:true,
-      nextText:'<span></span>',
-      prevText:'<span></span>',
-      onSlideBefore:function($slideElement, oldIndex, newIndex){
-        /*$j("#sliderThumbReal ul .active").removeClass("active");
-        $slideElement.addClass("active"); */
-
-      }
-    });
-
-    linkRealSliders(realSlider,realThumbSlider);
-
-    if($j("#bxslider-pager li").length<5){
-      $j("#bxslider-pager .bx-next").hide();
-    }
-
-    // sincronizza sliders realizzazioni
-    function linkRealSliders(bigS,thumbS){
-
-      $j("ul#bxslider-pager").on("click","a",function(event){
-        event.preventDefault();
-        var newIndex=$j(this).parent().attr("data-slideIndex");
-            bigS.goToSlide(newIndex);
-      });
-    }
-
-    //slider!=$thumbSlider. slider is the realslider
-    function changeRealThumb(slider,newIndex){
-
-      var $thumbS=$j("#bxslider-pager");
-      $thumbS.find('.active').removeClass("active");
-      $thumbS.find('li[data-slideIndex="'+newIndex+'"]').addClass("active");
-
-      if(slider.getSlideCount()-newIndex>=4)slider.goToSlide(newIndex);
-      else slider.goToSlide(slider.getSlideCount()-4);
-
-    }
-
-
     $('.mobile-slider').bxSlider({
         mode: 'horizontal',
         captions: true,
@@ -99,6 +28,12 @@ var realSlider= $j("ul#bxslider").bxSlider({
         speed:500
 
     });
+
+    // $('.news-slider').bxSlider({
+    //     pagerCustom: '#news-pager',
+    //     controls: false,
+    //     captions: true,
+    // });
 
      $('input').iCheck({
         checkboxClass: 'icheckbox_square-red',
@@ -131,6 +66,8 @@ var realSlider= $j("ul#bxslider").bxSlider({
             // }
         });
     }
+
+
 
 });
 
@@ -223,4 +160,72 @@ $(function(){
         root.find('.left-caret').toggleClass('right-caret left-caret');
         root.find('.sub-menu:visible').hide();
     });
+});
+
+
+$(document).ready(function() {
+// var $ = jQuery.noConflict();
+
+var realSlider= $("ul#bxslider").bxSlider({
+      speed:1000,
+      pager:false,
+      nextText:'',
+      prevText:'',
+      infiniteLoop:false,
+      hideControlOnEnd:true,
+      controls:false,
+      captions:true,
+      auto:true,
+      onSlideBefore:function($slideElement, oldIndex, newIndex){
+        changeRealThumb(realThumbSlider,newIndex);
+      }
+
+    });
+
+    var realThumbSlider=$("ul#bxslider-pager").bxSlider({
+      minSlides: 4,
+      maxSlides: 4,
+      slideWidth: 156,
+      slideMargin: 12,
+      moveSlides: 1,
+      pager:false,
+      speed:1000,
+      infiniteLoop:false,
+      hideControlOnEnd:true,
+      nextText:'<span></span>',
+      prevText:'<span></span>',
+      onSlideBefore:function($slideElement, oldIndex, newIndex){
+        /*$("#sliderThumbReal ul .active").removeClass("active");
+        $slideElement.addClass("active"); */
+
+      }
+    });
+
+    linkRealSliders(realSlider,realThumbSlider);
+
+    if($("#bxslider-pager li").length<5){
+      $("#bxslider-pager .bx-next").hide();
+    }
+
+    // sincronizza sliders realizzazioni
+    function linkRealSliders(bigS,thumbS){
+
+      $("ul#bxslider-pager").on("click","a",function(event){
+        event.preventDefault();
+        var newIndex=$(this).parent().attr("data-slideIndex");
+            bigS.goToSlide(newIndex);
+      });
+    }
+
+    //slider!=$thumbSlider. slider is the realslider
+    function changeRealThumb(slider,newIndex){
+
+      var $thumbS=$("#bxslider-pager");
+      $thumbS.find('.active').removeClass("active");
+      $thumbS.find('li[data-slideIndex="'+newIndex+'"]').addClass("active");
+
+      if(slider.getSlideCount()-newIndex>=4)slider.goToSlide(newIndex);
+      else slider.goToSlide(slider.getSlideCount()-4);
+
+    }
 });
